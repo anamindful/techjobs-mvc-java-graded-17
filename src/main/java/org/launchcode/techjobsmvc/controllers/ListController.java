02f9c9cd9227ed030d7b42900385e93ceddbd5aa
anamindful <anamindful@gmail.com>
2023-10-14 16:39:49 -0500

    website functions as needed. 3 tests are failing.

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -29,7 +29,7 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
-        tableChoices.put("all", Arrays.asList("All"));
+        tableChoices.put("all", Arrays.asList("View All"));
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
